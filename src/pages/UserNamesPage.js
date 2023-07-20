406266e9561000ce26c098edcf9948b12a377eae
anderson-nunes <andersonribeironunes@hotmail.com>
2023-07-20 11:34:46 -0300

    Atualizando dados

--- src/pages/UserNamesPage.js
+++ src/pages/UserNamesPage.js
@@ -1,44 +1,34 @@
-import { useState, useEffect } from "react";
-import { BASE_URL } from "../constants/constants";
-import axios from "axios";
-import {Title,NameContainer } from '../style'
+import { Title, NameContainer } from '../style'
 import { Card } from '../components/Card/Card'
-
+import { useRequestData } from '../hooks/useRequestData';
 
 const UserNamesPage = () => {
-  const [nomeUsuarios, setNomeUsuarios] = useState([]);
- 
 
-  useEffect(() => {
-    axios
-      .get(`${BASE_URL}users`)
-      .then((response) => {
-        setNomeUsuarios(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
+  const [nomeUsuarios, isLoading, isError] = useRequestData('users')
 
   return (
     <div>
       <Title>Nomes dos usuários</Title>
       <NameContainer>
-        {nomeUsuarios.map((usuario) => {
-          return(
-          <Card 
-          key={usuario.id} 
-          text={usuario.name} 
-          backgroudColor={'nome'}
-          textColor={'nome'}
-          />)
-        })}
+        {
+          isError ? <p>ERRO! TENTE NOVAMENTE</p> :
+            isLoading ? <p>CARREGANDO...</p> :
+
+              nomeUsuarios.map((usuario) => {
+                return (
+                  <Card
+                    key={usuario.id}
+                    text={usuario.name}
+                    backgroudColor={'nome'}
+                    textColor={'nome'}
+                  />)
+              })}
       </NameContainer>
     </div>
   );
 }
 
-export default  UserNamesPage;
+export default UserNamesPage;
 
 
 
